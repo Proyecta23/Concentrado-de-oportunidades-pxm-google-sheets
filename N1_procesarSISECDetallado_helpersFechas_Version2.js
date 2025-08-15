02a8a0a0918f681a04c1ac47893718d65292d64b
Proyecta x mas <131374356+Proyecta23@users.noreply.github.com>
2025-08-14 20:43:47 -0600

    Add files via upload

---
+++ N1_procesarSISECDetallado_helpersFechas_Version2.js
@@ -0,0 +1,17 @@
+function restarUnDia(fecha) {
+    let partes = fecha.split("/");
+    let dateObj = new Date(parseInt(partes[2]), parseInt(partes[1]) - 1, parseInt(partes[0]));
+    dateObj.setDate(dateObj.getDate() - 1);
+
+    let dia = ("0" + dateObj.getDate()).slice(-2);
+    let mes = ("0" + (dateObj.getMonth() + 1)).slice(-2);
+    let anio = dateObj.getFullYear();
+
+    return `${dia}/${mes}/${anio}`;
+}
+
+function obtenerUltimoDiaMes() {
+    let hoy = new Date();
+    let ultimoDia = new Date(hoy.getFullYear(), hoy.getMonth() + 1, 0);
+    return Utilities.formatDate(ultimoDia, Session.getScriptTimeZone(), "dd/MM/yyyy");
+}
